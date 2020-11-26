35075839f696148cf820b6cb9ecc388bca6a3758
Dmitri Petchonkin <dmitri.petchonkin@intelometry.com>
2020-11-26 17:13:11 +0200

    reports controller.

---
+++ LinnWorks/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LinnWorks.Classes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinnWorks.Controllers
+{
+	[Route( "[controller]" )]
+	[ApiController]
+	public class ReportController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<ReportModel> Get()
+		{
+			List<ReportModel> toReturn = new List<ReportModel>();
+			return toReturn;
+		}
+	}
+}
